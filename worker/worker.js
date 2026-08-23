1956c6fcef7e0f68072f2b1a71b95c066696c8f7
Mo <mo@iptv.com>
2026-08-23 22:12:08 +0000

    Update Trex server URL to mag.trexlive.me

--- worker/worker.js
+++ worker/worker.js
@@ -8,7 +8,7 @@
 
 const API_BASE    = "https://activationpanel.ru/api/api.php";
 const API_KEY     = "35cf68cc83a3a82e1a0ac5361c7b6105";
-const HOST        = "http://terry.thecontentnest.com";
+const HOST        = "http://mag.trexlive.me";
 const RESEND_KEY  = "re_QtGJLR7i_AnoxwiyK67LsFXSkxYEHzrR4";
 const FROM_EMAIL  = "Maple Stream TV <help@maplestreamtv.ca>";
 const ADMIN_EMAIL = "help@maplestreamtv.ca";
